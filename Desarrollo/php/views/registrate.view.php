5b3d35f8c2ef29115e4665bb17e717f770227d08
CarlosNitsuga <CarlosNitsuga@Upper>
2018-10-03 00:42:47 -0500

    modificacion a registrate.view

--- Desarrollo/php/views/registrate.view.php
+++ Desarrollo/php/views/registrate.view.php
@@ -2,7 +2,7 @@
 <html lang="en">
   <head>
     <meta charset="utf-8">
-    <title>Formulario Contacto</title>
+    <title>Registro</title>
     <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
     <link rel="stylesheet" href="css/estilo1.css">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
