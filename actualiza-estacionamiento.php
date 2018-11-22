56bcb8230e93e1cfd14fc87857884d50176009ff
Iscbr <israelscbr@gmail.com>
2018-11-21 20:24:25 -0600

    Actualizado

--- actualiza-estacionamiento.php
+++
@@ -1,7 +0,0 @@
-<?php
-  include ('functions.php');
-  
-  $id_estacionamiento = $_GET['id_estacionamiento'];
-  $OCUPADOS=$_GET['ocupados'];
-  ejecutarSQLCommand("UPDATE estacionamientos SET ocupados='$OCUPADOS' WHERE id_estacionamiento= '$id_estacionamiento'");
- ?>
--- consulta-estacionamiento-id.php
+++
@@ -1,9 +0,0 @@
-<?php
-include('functions.php');
-$id_estacionamiento = $_GET["id_estacionamiento"];
-if($resultset=getSQLResultSet("SELECT * FROM `estacionamientos` WHERE `id_estacionamiento` = '$id_estacionamiento' ORDER BY `nombre` ASC")){
-	while ($row = $resultset->fetch_array(MYSQLI_NUM)){
-		echo json_encode($row);
-	}
-}
-?>
